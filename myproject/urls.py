09f29c07c083b179cd1ddf4a96c529afb65fd012
Shubham Kumar <sg8900084@gmail.com>
2025-12-04 12:11:32 +0530

    fevicon

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     path('accounts/', include('allauth.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
