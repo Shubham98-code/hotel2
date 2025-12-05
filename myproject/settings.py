adb8f72e731c865c8738b879434e2b3447cd922f
Shubham Kumar <sg8900084@gmail.com>
2025-12-05 16:28:54 +0530

    Switch email to SSL port 465

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,12 +117,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# --- EMAIL CONFIGURATION ---
+# --- EMAIL CONFIGURATION (Updated for Render) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+
+# Change to Port 465 and SSL
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False  # Turn this OFF
+EMAIL_USE_SSL = True   # Turn this ON
+
 EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
