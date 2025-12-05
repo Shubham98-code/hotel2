11d0e1008a9fec4ec14932ac3051ff4ee5e483d7
Shubham Kumar <sg8900084@gmail.com>
2025-12-05 16:15:20 +0530

    admin

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -o errexit
 # 1. Install Requirements
 pip install -r requirements.txt
 
-# 2. Collect Static Files (CSS/JS)
+# 2. Collect Static Files
 python manage.py collectstatic --no-input
 
-# 3. Migrate Database (Fixes the 500 Error by creating tables)
+# 3. Migrate Database
 python manage.py migrate
 
-# 4. Create Superuser automatically (from Environment Variables)
-# The "|| true" part prevents the build from failing if the user already exists
-if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+# 4. Create Superuser (Corrected Syntax)
+# We use single brackets [ ] which works in all shells
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
     python manage.py createsuperuser --no-input || true
 fi
